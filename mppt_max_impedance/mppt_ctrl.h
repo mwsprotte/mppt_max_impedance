76857f8feab8bd1421c54bd6c0849cec77d14e3a
Matheus Willian Sprotte <msprotte@outlook.com>
2023-11-07 10:34:13 -0300

    remove comments

--- mppt_max_impedance/mppt_ctrl.h
+++ mppt_max_impedance/mppt_ctrl.h
@@ -14,83 +14,21 @@ void mppt() {
   Serial.println("Potência = " + (String)Pot + " W");
 
   voltageDiff = voltage[k] - voltage[k - 1];  //Calculando o ∆V
-  // if (voltageDiff == 0.0) {
-  //   voltageDiff = 0.001;
-  // }
 
   z_inDiff = z_in[k] - z_in[k - 1];
 
-  // z_inDiff = abs(z_inDiff);
-  // voltageDiff = abs(voltageDiff);
-
   Serial.println("Delta V = " + (String)voltageDiff + " V");
   Serial.println("Zdif = " + (String)z_inDiff);
-  // Serial.println("Zatual = " + (String)z_in[k]);
-
-
-  // if (voltageDiff == 0) {
-  //   Pot_rate[k] = -999999999;
-  //   usePotRate = false;
-  // } else if (voltageDiff == -0) {
-  //   Pot_rate[k] = +999999999;
-  //   usePotRate = false;
-  // } else if (z_inDiff == 0) {
-  //   Pot_rate[k] = (2 * voltage[k] / z_in[k]);
-  //   usePotRate = false;
-  // } else if (z_inDiff == -0) {
-  //   Pot_rate[k] = (2 * voltage[k] / z_in[k]);
-  //   usePotRate = false;
-  // } else {
-  //   Pot_rate[k] = (2 * voltage[k] / z_in[k]) - ((pow(voltage[k], 2) / pow(z_in[k], 2)) * (z_inDiff / voltageDiff));  //Calculando o ∆V/∆P
-  //   usePotRate = true;
-  // }
-
   Pot_rate[k] = (2 * voltage[k] / z_in[k]) - ((pow(voltage[k], 2) / pow(z_in[k], 2)) * (z_inDiff / voltageDiff));  //Calculando o ∆V/∆P
   // delay(1000);
 
-  // if (!isDigit(Pot_rate[k])) {
-  //   Pot_rate[k] = Pot_rate[k - 1];
-  // }
-  // if ((Pot_rate[k])=="NAN") {
-  //   Pot_rate[k] = Pot_rate[k - 1];
-  // }
-
-
-
   Serial.println("Resultado da eq. de rastreamento = " + (String)Pot_rate[k]);
-
-  // if (usePotRate) {
-  //   if (Pot_rate[k] > 0) {
-  //     // if (Pot_rate[k] <= 0) {
-  //     D[k] = D[k - 1] - delta;
-  //     // D[k] = D[k - 1] + delta;
-  //   } else {
-  //     D[k] = D[k - 1] + delta;
-  //     // D[k] = D[k - 1] - delta;
-  //   }
-  // } else {
-  //   D[k] = D[k - 1];
-  // }
-
   if (Pot_rate[k] > 0) {
-    // if (Pot_rate[k] <= 0) {
+
     D[k] = D[k - 1] - delta;
-    // D[k] = D[k - 1] + delta;
   } else {
     D[k] = D[k - 1] + delta;
-    // D[k] = D[k - 1] - delta;
   }
-
-  // Alterando o duty cycle beaseado nas leituras
-  // if (Pot_rate[k] > 0) {
-  //   D[k] = D[k - 1] - delta;
-  //   // D[k] = D[k - 1] + delta;
-  // } else if (Pot_rate[k] == 0.00) {
-  //   Pot_rate[k] = Pot_rate[k - 1];
-  // } else {
-  //   D[k] = D[k - 1] + delta;
-  //   // D[k] = D[k - 1] - delta;
-  // }
 
   // Limitando no novo duty cycle os valores máximos e mínimos
   if (D[k] < 20) {
